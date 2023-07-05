f0c7022b793ea3954c19a0493d589ec6a3f6bd55
Kaushik Nitin Naik <kaushik.naik1602@gmail.com>
2023-07-05 17:57:30 +0530

    Add new subject

--- app.js
+++ app.js
@@ -105,17 +105,28 @@ app.post("/login", function (req, res) {
 
 app.get("/stats", function (req, res) {
   Quiz.find({}).then((res1) => {
-    Quiz.find({ subject: "maths" }).then((res2) => {
-      Quiz.find({ subject: "english" }).then((res3) => {
-        Quiz.find({ subject: "science" }).then((res4) => {
-          res.status(200).json({
-            all: res1.length,
-            maths: res2.length,
-            english: res3.length,
-            science: res4.length,
-          });
-        });
-      });
+    let all = res1.length,
+      eng = 0,
+      maths = 0,
+      it = 0,
+      marathi = 0;
+    for (let i = 0; i < res1.length; i++) {
+      if (res1[i].subject === "maths") {
+        maths = maths + 1;
+      } else if (res1[i].subject === "english") {
+        eng = eng + 1;
+      } else if (res1[i].subject === "it") {
+        it = it + 1;
+      } else {
+        marathi = marathi + 1;
+      }
+    }
+    res.status(200).json({
+      all: all,
+      english: eng,
+      it: it,
+      maths: maths,
+      marathi: marathi,
     });
   });
 });
