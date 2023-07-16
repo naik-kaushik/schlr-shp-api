f56d677eefa320fd69e40fa01d879a354b96c600
Kaushik Naik <87433036+naik-kaushik@users.noreply.github.com>
2023-07-16 15:39:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -103,7 +103,10 @@ app.post("/login", function (req, res) {
       res.status(200).json({ message: err });
     } else {
       passport.authenticate("local")(req, res, function () {
-        res.status(200).json({status:true,user : DetUser.findOne({username : user.username})});
+        DetUser.findOne({username : user.username}).then(foundUser=>{
+           res.status(200).json({status:true,user : foundUser});
+        })
+       
       });
     }
   });
